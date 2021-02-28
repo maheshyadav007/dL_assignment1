983815db8129ed6967a514d6e262d628f8926353
maheshyadav007 <maheshyadavindian@gmail.com>
2021-02-28 13:50:23 +0530

    Added function to initialize NN

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import wandb
 from keras.datasets import fashion_mnist
 
-wandb.init(project="dl_assignment1", entity="-my")
+#wandb.init(project="dl_assignment1", entity="-my")
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 classLabels = {"0":"T-shirt/top","1":"Trouser","2":"Pullover","3":"Dress","4":"Coat","5":"Sandal","6":"Shirt","7":"Sneaker","8":"Bag","9":"Ankle boot"}
@@ -17,3 +17,29 @@ def outEachClass():
     print(len(data))
     wandb.log({"Categories": [wandb.Image(image, caption=caption) for image, caption in zip(data,labels)]})
 
+n,l,w = x_train.shape
+nFeatures = l*w
+X = np.reshape(x_train,(n,l*w))
+Y = y_train
+
+def initializeParams(nFeatures,weights,bias,nHiddenLayers,neurons):
+    n = nFeatures
+    for i in range(nHiddenLayers+1):
+        weights.append(np.random.rand(n,neurons[i]))
+        bias.append(np.random.rand(neurons[i]))
+        n = neurons[i]
+def initializeNN(): 
+    weights = []
+    bias = []
+    nHiddenLayers = 2
+    neurons = [2, 1]
+    outputNeurons = 10
+    neurons.append(outputNeurons)
+    initializeParams(nFeatures,weights,bias,nHiddenLayers,neurons)
+
+    return weights, bias
+
+weights, bias = initializeNN()
+    
+print(weights)
+print(bias)
